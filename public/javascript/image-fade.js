5c9f3f7b2792d1a900aba9d2d0d2279d0d969295
Claire Sylvester <clairesylvstr@gmail.com>
2020-12-16 20:00:50 -0500

    absolute alignment

--- public/javascript/image-fade.js
+++ public/javascript/image-fade.js
@@ -1,6 +1,6 @@
-$(".card").hover(function() {
-    $(this).find(".circle").stop(true).slideUp(1000); 
-},
-function() { 
-    $(this).find(".circle").stop(true).slideDown(1000);
-}); 
+// $(".card").hover(function() {
+//     $(this).find(".circle").stop(true).slideUp(1000); 
+// },
+// function() { 
+//     $(this).find(".circle").stop(true).slideDown(1000);
+// }); 
